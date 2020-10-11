2764418c3a51c4e5a9aa5a6f46b3219700abd0ec
Nick Muller <muller_nicky@hotmail.com>
2020-10-11 23:42:34 +0200

    initial commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace tempdata_cookie_redirect.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+			TempData["tempdata-redirect-test"] = "hi " + new Random().Next();
+        }
+		
+		public IActionResult OnPost()
+		{
+			var _ = TempData["tempdata-redirect-test"];
+			return RedirectToPage("/Index");
+		}
+    }
+}
